29c23662dd2d3587a74ca53ed598892bde9903db
Jörg Kantel <joerg@kantel.de>
2019-01-25 20:12:49 +0100

    Template aufgehübscht

--- framework/framework.py
+++ framework/framework.py
@@ -7,14 +7,21 @@ import pygame as pg
 from pygame.locals import *  # Wenn dies nicht importiert wird,
                              # kann man UTF-8 (Umlaute) knicken
 import os
+
+# Einige nützliche Konstanten
+WIDTH = 640
+HEIGHT = 480
+FPS = 30   # Framerate
+
+# Pygame initialisieren und das Fenster und die Hintergrundfarbe festlegen
 pg.init()
-
-win = pg.display.set_mode((640, 480))
+win = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Hallo Wörld!")
-
 background = pg.Surface(win.get_size())
 background = background.convert()
 background.fill((0, 80, 125))
+clock = pg.time.Clock()
+
 
 # Assets laden
 # Hier wird der Pfad zum Verzeichnis des ».py«-Files gesetzt
@@ -23,15 +30,22 @@ background.fill((0, 80, 125))
 file_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(file_path)
 
-clock = pg.time.Clock()
-clock.tick(30)  # Framerate
-
 keep_going = True
 while keep_going:
+    # Framerate feslegen
+    clock.tick(FPS)
+    # Eingabe verarbeiten (Input Events)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             keep_going = False
 
+    # Update
+
+    # Draw
     win.blit(background, (0, 0))
+    
+    # Wenn *alles* gezeichnet ist
     pg.display.flip()
-            
+
+print("I did it, Babe!")
+pg.quit()          
