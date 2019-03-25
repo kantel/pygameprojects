515d5fbc2d44f4cff261193fb10ce5618d254f6a
Jörg Kantel <joerg@kantel.de>
2019-03-25 13:38:33 +0100

    Update cutespace.py

--- pgzero/cutespace/cutespace.py
+++ pgzero/cutespace/cutespace.py
@@ -23,35 +23,54 @@ planets[1].pos = (1025, 220)
 
 score = 0
 game_over = False
+up = False
 
 def reset(actor):
     actor.x = randint(700, 2100)
     actor.y = randint(40, 440)
 
 def draw():
-    screen.fill((0, 80, 125))
-    for planet in planets:
-        planet.draw()
-    octopussy.draw()
-    for rocketboy in rocketboys:
-        rocketboy.draw()
+    if not game_over:
+        screen.fill((0, 80, 125))
+        for planet in planets:
+            planet.draw()
+            octopussy.draw()
+            for rocketboy in rocketboys:
+                rocketboy.draw()
+        screen.draw.text("Punkte: " + str(score), (10, 10), color = "white")
+    else:
+        screen.fill((0, 80, 125))
+        screen.draw.text("GAME OVER!", (WIDTH/2 - 50, HEIGHT/2), color = "white")
 
 def update():
     global game_over, score
     if not game_over:
-        octopussy.y += 0
+        if not up:
+            octopussy.y += 1
         for rocketboy in rocketboys:
             if rocketboy.x > -rocketboy.width:
                 rocketboy.x -= rocketboy.speed
             else:
+                score += 1
                 reset(rocketboy)
         for planet in planets:
             if planet.x > -planet.width:
                 planet.x -= 0.25 
             else:
                 reset(planet)
+        if octopussy.top < 0 or octopussy.bottom > HEIGHT:
+            game_over = True
+        for rocketboy in rocketboys:
+            if octopussy.collidepoint(rocketboy.x, rocketboy.y):
+                game_over = True
     
 def on_mouse_down():
-    pass
+    global up
+    up = True
+    octopussy.y -= 50
+
+def on_mouse_up():
+    global up
+    up = False
     
 pgzrun.go()
