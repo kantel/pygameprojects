8aa83800d7de9b3c4e522db4a6a114fc138e0a3e
Jörg Kantel <joerg@kantel.de>
2021-01-27 21:28:31 +0100

    Create mediadaten.py

---
+++ matplotlib/mediadaten.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+months = ["02/20", "03/20", "04/20", "05/20", "06/20", "07/20",
+          "08/20", "09/20", "10/20", "11/20", "12/20", "01/21"]
+pageviews = [13968, 14989, 16562, 17272, 12609, 10940, 13701, 11577, 12224, 13520, 13849, 14355]
+visitors  = [6611, 7370, 7848, 7855, 6545, 5681, 5639, 5864, 6242, 7121, 6987, 6932]
+
+plt.figure(figsize = (8, 4))
+plt.plot(months, pageviews, label = "Pageviews")
+plt.plot(visitors, label = "Visitors")
+plt.grid(True)
+plt.ylim(0, 20000)
+plt.legend()
+
+plt.show()
