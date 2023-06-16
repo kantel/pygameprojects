9dc0c0eece7a10652bfe28a9443614637a086ee7
Jörg Kantel <joerg@kantel.de>
2023-06-16 20:24:41 +0200

    Simple Platformer v02

--- platformer/main.py
+++ platformer/main.py
@@ -1,6 +1,8 @@
-# Einfacher Platformer, Version 0.1
+# Einfacher Platformer, Version 0.2
+# Nach einer Idee von Jonathan Cooper
+# (https://www.youtube.com/playlist?list=PLk64HPu_u_NOD0hC2XFZfDu1ZOr7OF1qB)
 # Sprites und Tiles: Pixel Platformer (CC0) von Kenney.nl
-# https://www.kenney.nl/assets/pixel-platformer
+# (https://www.kenney.nl/assets/pixel-platformer)
 # Jörg Kantel, 2023
 import pygame as pg
 import asyncio
@@ -17,10 +19,11 @@ FPS = 60                     # Frames per second
 PLAYER_WIDTH = 24
 PLAYER_HEIGHT = 24
 PLAYER_START_X, PLAYER_START_Y = 5, 1
-PLAYER_SPEED = 5
+PLAYER_SPEED = 3
 
 # Physikalische Konstanten
 GRAVITY = 0.5
+JUMP_POWER = 10
 
 ## Hier wird der Pfad zum Verzeichnis der Assets gesetzt
 IMAGEPATH = os.path.join(os.getcwd(), "data/images")
@@ -46,6 +49,7 @@ class GameWorld:
         ## Load Assets
         grass_image = pg.image.load(os.path.join(IMAGEPATH, "grass_02.png")).convert_alpha()
         block_image = pg.image.load(os.path.join(IMAGEPATH, "block_00.png")).convert_alpha()
+        gem_image   = pg.image.load(os.path.join(IMAGEPATH, "gem.png")).convert_alpha()
 
         grass_locations = []
         for i in range(GRID_WIDTH):
@@ -60,7 +64,9 @@ class GameWorld:
             self.platforms.add(p)
             self.all_sprites.add(p)
     
-        block_locations = [(11, 7), (12, 7), (13, 7), (14, 7),
+        block_locations = [(18, 4), (19, 4), (20, 4), (21, 4),                         
+                           (11, 7), (12, 7), (13, 7), (14, 7),
+                           (25, 7), (26, 7), (27, 7),
                            (17, 10), (18, 10), (19, 10),
                            (38, 12), (38, 13)]
         for loc in block_locations:
@@ -69,6 +75,16 @@ class GameWorld:
             p = Platform(x, y, block_image)
             self.platforms.add(p)
             self.all_sprites.add(p)
+
+        gem_locations = [(20,3), (12, 6), (26,6), (36, 13)]
+        self.items = pg.sprite.Group()
+        for loc in gem_locations:
+            x = loc[0]
+            y = loc[1]
+            g = Gem(x, y, gem_image)
+            self.items.add(g)
+            self.all_sprites.add(g)
+
 
         self.player = Player(PLAYER_START_X, PLAYER_START_Y)
         self.player_sprite_group = pg.sprite.GroupSingle()
@@ -117,6 +133,19 @@ class Platform(pg.sprite.Sprite):
         self.rect.y = _y*GRIDSIZE
 ## End Class Platform
 
+class Gem(pg.sprite.Sprite):
+
+    def __init__(self, _x, _y, _image):
+        super().__init__()
+        self.image = _image
+        self.rect = self.image.get_rect()
+        self.rect.x = _x*GRIDSIZE
+        self.rect.y = _y*GRIDSIZE
+    
+    def apply(self, character):
+        character.gems += 1
+## End Class Gem
+
 ## Class Player
 class Player(pg.sprite.Sprite):
 
@@ -132,11 +161,17 @@ class Player(pg.sprite.Sprite):
         self.rect.bottom = _y*GRIDSIZE
         # self.rect.topleft = (self.rect.x, self.rect.y)
         self.speed = PLAYER_SPEED
+        self.jump_power = JUMP_POWER
         self.vx = 0
         self.vy = 0
+        self.gems = 0
     
     def jump(self):
-        pass
+        self.rect.y += 2
+        hits = pg.sprite.spritecollide(self, world.platforms, False)
+        self.rect.y -= 2
+        if len(hits) > 0:
+            self.vy = -1*self.jump_power
 
     def apply_gravity(self):
         self.vy += GRAVITY
@@ -149,6 +184,8 @@ class Player(pg.sprite.Sprite):
         elif keys[pg.K_d]:      # RIGHT
             if self.rect.x < WIDTH - PLAYER_WIDTH:
                 self.vx = self.speed
+        elif keys[pg.K_w]:      # JUMP
+            self.jump()
         else:
             self.vx = 0
         # Horizonfale Kollision
@@ -157,7 +194,7 @@ class Player(pg.sprite.Sprite):
         for hit in hits:
             if self.vx > 0:
                 self.rect.right = hit.rect.left
-            if self.vx < 0:
+            elif self.vx < 0:
                 self.rect.left = hit.rect.right
         # Vertikale Kollision
         self.rect.y += self.vy
@@ -165,6 +202,8 @@ class Player(pg.sprite.Sprite):
         for hit in hits:
             if self.vy > 0:
                 self.rect.bottom = hit.rect.top
+            elif self.vy < 0:
+                self.rect.top = hit.rect.bottom
             self.vy = 0
 
     def check_edges(self):
@@ -172,11 +211,18 @@ class Player(pg.sprite.Sprite):
             self.rect.left = 0
         elif self.rect.right > WIDTH:
             self.rect.right = WIDTH
+    
+    def check_items(self):
+        hits = pg.sprite.spritecollide(self, world.items, True)
+        for item in hits:
+            item.apply(self)
+            print(self.gems)   # Nur für Testzwecke
 
     def update(self):
         self.apply_gravity()
         self.move()
         self.check_edges()
+        self.check_items()
 
 ## End Class Player
 
