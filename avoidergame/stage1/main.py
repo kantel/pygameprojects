ca8e8cee3c211c9a935722baad83a13f391ad4d1
Jörg Kantel <joerg@kantel.de>
2023-06-24 13:49:54 +0200

    avoider stage 1

--- avoidergame/stage1/main.py
+++ avoidergame/stage1/main.py
@@ -22,7 +22,7 @@ DANGER_ZONE = 360
 DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
 ## Farben
-BG_COLOR = (0, 0, 0)
+BG_COLOR = (128, 57, 52)
 
 # Klassen
 ## Class GameWorld
@@ -42,6 +42,7 @@ class GameWorld:
         self.smiley0_image = pg.image.load(os.path.join(DATAPATH, "smiley0.png")).convert_alpha()
         self.smiley1_image = pg.image.load(os.path.join(DATAPATH, "smiley1.png")).convert_alpha()
         self.smiley2_image = pg.image.load(os.path.join(DATAPATH, "smiley4.png")).convert_alpha()
+        self.score_font = pg.font.Font(os.path.join(DATAPATH, "comichelvetic_medium.ttf"), 25)
 
         # Game State
         self.start_game = True
@@ -58,6 +59,7 @@ class GameWorld:
             self.all_sprites.add(enemy)        
         self.player = Player(self)
         self.all_sprites.add(self.player)
+        self.hud = HUD(self)
           
     def events(self):
         for event in pg.event.get():
@@ -70,10 +72,12 @@ class GameWorld:
  
     def update(self):
         self.all_sprites.update()
+        self.hud.update(self.player.score)
 
     def draw(self):
         self.screen.fill(BG_COLOR)
         self.all_sprites.draw(self.screen)
+        self.screen.blit(self.hud.score, self.hud.score_rect)
         pg.display.flip()
     
     def start_screen(self):
@@ -93,6 +97,7 @@ class Player(pg.sprite.Sprite):
         self.image = self.game_world.skull_image
         self.rect = self.image.get_rect()
         self.x, self.y = WIDTH/2, PLAYER_Y
+        self.score = 0
     
     def update(self):
         x, y = pg.mouse.get_pos()
@@ -127,6 +132,8 @@ class Enemy(pg.sprite.Sprite):
         self.rect.center = (self.x, self.y)
         if self.rect.top >= HEIGHT:
             self.over = True
+            self.game_world.player.score += 1
+            # print(self.game_world.player.score)
             self.reset()
     
     def reset(self):
@@ -134,6 +141,21 @@ class Enemy(pg.sprite.Sprite):
         self.y = -randint(50, 250)
         self.rect.center = (self.x, self.y)
         self.dy = randint(SPEED_MIN, SPEED_MAX)
+
+class HUD():
+
+    def __init__(self, _world):
+        self.game_world = _world
+        self.score_x = 30
+        self.score_y = 15
+        self.score_font = self.game_world.score_font
+        self.score = ""
+    
+    def update(self, points):
+        self.score = self.score_font.render(f"Score: {points}", True, (255, 255, 255))
+        self.score_rect = self.score.get_rect()
+        self.score_rect.x = self.score_x
+        self.score_rect.y = self.score_y
 
 # Hauptprgramm
 world = GameWorld()
