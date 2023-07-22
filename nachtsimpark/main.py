d252f99927ceb9b1e271155cc1e14405f19458c5
Jörg Kantel <joerg@kantel.de>
2023-07-22 21:03:21 +0200

    Nachts im Park Stage 1

--- nachtsimpark/main.py
+++ nachtsimpark/main.py
@@ -5,18 +5,7 @@ import pygame as pg
 import asyncio
 from pygame.locals import *
 import os, sys
-
-## Settings
-GRIDSIZE = 16
-GRID_WIDTH = 30
-GRID_HEIGHT = 20
-WIDTH, HEIGHT = GRID_WIDTH*GRIDSIZE, GRID_HEIGHT*GRIDSIZE
-TITLE = "A Night in a Secret Garden"
-FPS = 60           # Frames per second
-PLAYER_START_X, PLAYER_START_Y = 28, 20
-
-## Hier wird der Pfad zum Verzeichnis der Assets gesetzt
-IMAGEPATH = os.path.join(os.getcwd(), "data/images")
+from settings import *
 
 ## Class GameWorld
 class GameWorld:
@@ -28,12 +17,26 @@ class GameWorld:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.keep_going = True
+        # pg.key.set_repeat(0, 0)
+
 
     def reset(self):
         # Neustart oder Status zurücksetzen
         # Hier werden alle Elemente der GameWorld initialisiert
-        pass
         ## Load Assets
+        self.bg_image = pg.image.load(os.path.join(IMAGEPATH, "secretpark.png")).convert_alpha()
+        ## Load Walls
+        room = room_01
+        self.walls = []
+        for y in range(GRID_HEIGHT):
+            for x in range(GRID_WIDTH - 10):
+                if room[y][x] >= 60:
+                    self.walls.append((x, y))
+        # print(self.walls)
+        ## Sprites
+        self.all_sprites = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
 
     def events(self):
         for event in pg.event.get():
@@ -42,18 +45,58 @@ class GameWorld:
                 and event.key == pg.K_ESCAPE)):
                 self.keep_going = False
                 self.game_over()
+            
+            if event.type == pg.KEYDOWN:
+                if (event.key == pg.K_w or event.key == pg.K_UP):
+                    self.player.dy = -1 
+                elif (event.key == pg.K_s or event.key == pg.K_DOWN):
+                    self.player.dy = 1
+                elif (event.key == pg.K_a or event.key == pg.K_LEFT):
+                    self.player.dx = -1 
+                elif (event.key == pg.K_d or event.key == pg.K_RIGHT):
+                    self.player.dx = 1
+            
+            if event.type == pg.KEYUP:
+                self.player.dx, self.player.dy = 0, 0
 
     def update(self):
-        pass
+        self.all_sprites.update()
 
     def draw(self):
+        self.screen.fill(BG_COLOR)
+        self.screen.blit(self.bg_image, (0, 0))
+        self.all_sprites.draw(self.screen)
         pg.display.flip()
 
     def game_over(self):
-        # print("Bye, Bye, Baby!")
+        print("Bye, Bye, Baby!")
         pg.quit()
         sys.exit()
 ## Ende Class GameWorld
+
+## Class Player
+class Player(pg.sprite.Sprite):
+
+    def __init__(self, _world):
+        self.world = _world
+        super().__init__()
+        # Load Image
+        img = pg.image.load(os.path.join(IMAGEPATH, "hero.png")).convert_alpha()
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.x, self.y = PLAYER_START_X*GRIDSIZE, PLAYER_START_Y*GRIDSIZE
+        self.dx, self.dy = 0, 0
+
+    def update(self):
+        if (self.x + self.dx, self.y + self.dy) not in world.walls:
+            self.x += self.dx
+            self.y += self.dy
+        else:
+            self.x += 0
+            self.y += 0
+        self.rect.x, self.rect.y = self.x, self.y
+
+## End Class Player
 
 # Hauptprogramm
 world = GameWorld()
